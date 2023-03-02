073b694b9415b5f1a91e2e48bb3c864d479420ee
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-03-03 02:17:20 +0530

    Netlify Graph update

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+/** Here's how to say Hello to Netlify Graph! */
+await fetch('https://graph.netlify.com/graphql?app_id=b83d3c66-7e5b-47e6-96b4-5ef3cc6f57b6&show_metrics=false',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `query HelloNetlifyGraph {
+    helloGraph(sessionId: "78213f69-a5c6-4a54-980b-25f0ecc7495b") {
+      welcome
+      about
+      whatToDoNext
+    }
+  }`,
+    }),
+  }
+)
+.then((r) => r.json())
